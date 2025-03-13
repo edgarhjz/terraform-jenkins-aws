22e11d98195640e5e87c90cc152c3a8e3280d94f
Edgar Hernandez <iedgarus@gmail.com>
2025-03-12 23:14:24 -0600

    adding the resources to main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,57 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf update -y
+              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo dnf upgrade -y
+              sudo dnf install fontconfig -y
+              sudo dnf install java-17-amazon-corretto-headless -y
+              sudo dnf install jenkins -y
+              sudo systemctl daemon-reload
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
+              EOF
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Security group for Jenkins server"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
