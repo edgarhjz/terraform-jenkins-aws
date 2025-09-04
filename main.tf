de96f414f9b8f7b2b837a210f4c6232780e0312e
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-04 11:28:18 -0600

    Add public subnet

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ resource "aws_vpc" "jenkins_vpc" {
   }
 }
 
+resource "aws_subnet" "jenkins_public_subnet" {
+  vpc_id            = aws_vpc.jenkins_vpc.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
 resource "aws_instance" "jenkins" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
--- variables.tf
+++ variables.tf
@@ -4,6 +4,19 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "vpc_cidr" {
+  type = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+  
+}
+
+variable "subnet_cidr" {
+  type = string
+  description = "CIDR block for the subnet"
+  default     = "10.0.0.1/24"
+}
+
 variable "ami_id" {
   type = string
   description = "AMI ID for the instance"
