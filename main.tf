47ff915f5ca4832995a1aee4f13c3211de33e62a
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-04 11:32:49 -0600

    Add route table

--- main.tf
+++ main.tf
@@ -24,58 +24,14 @@ resource "aws_internet_gateway" "jenkins_igw" {
   }
 }
 
-resource "aws_instance" "jenkins" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+resource "aws_route_table" "jenkins_rtb" {
+  vpc_id = aws_vpc.jenkins_vpc.id
 
-  user_data = <<-EOF
-              #!/bin/bash
-              exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-
-              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-              sudo dnf upgrade -y
-
-              sudo dnf install fontconfig java-21-openjdk -y
-              sudo dnf install jenkins -y
-
-              sudo systemctl daemon-reload
-              sudo systemctl enable jenkins
-              sudo systemctl start jenkins
-              EOF
-
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+  }
   tags = {
-    Name = "jenkins-server"
-  }
-}
-
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Security group for Jenkins server"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "jenkins-sg"
+    Name = "jenkins-rtb"
   }
 }
