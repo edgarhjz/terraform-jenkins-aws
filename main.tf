daa44beed0bef6999480c695204ab5895cfb2b70
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-04 11:14:17 -0600

    Add providers.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_instance" "jenkins" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
