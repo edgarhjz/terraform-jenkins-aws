f242c6a1ec17f395eaff4c8d35e48bc7ba8f64ba
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-04 11:21:41 -0600

    Add backend

--- providers.tf
+++ providers.tf
@@ -5,9 +5,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+  bucket         = "tf-backend-project1"
+  key            = "tf-jenkins/terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "tf-lock-table"
+  encrypt        = true
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
