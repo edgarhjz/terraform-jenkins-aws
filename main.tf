53b2af5fd91c140cce3960c8b43e138d05807b18
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-04 11:36:40 -0600

    Add security group

--- main.tf
+++ main.tf
@@ -40,3 +40,32 @@ resource "aws_route_table_association" "jenkins_rtb_assoc" {
   subnet_id      = aws_subnet.jenkins_public_subnet.id
   route_table_id = aws_route_table.jenkins_rtb.id
 }
+
+resource "aws_security_group" "jenkins_sg" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
